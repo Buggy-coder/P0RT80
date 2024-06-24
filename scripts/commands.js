54d3c88a4afe5ab30fccf30be467cb9a65911a6c
Buggy-coder <78601438+Buggy-coder@users.noreply.github.com>
2024-06-24 17:53:49 +0530

    commit

---
+++ scripts/commands.js
@@ -0,0 +1,64 @@
+var youtube = "https://www.youtube.com/@buggy_coder/";
+var twitter = "https://www.twitter.com/http_abhinav/";
+var password = "EasyToCrack";
+var linkedin = "https://www.linkedin.com/in/abhinav-io/";
+var github = "https://github.com/CodeinCommand/";
+var email = 'mailto:abhinav17ap@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Abhinav Patel!👋",
+  "I'm a sophomore B.Tech CSE student at UPES",
+  "I am passionate about computer science & software engineering.",
+  "I am on a mission to absorb knowledge and grow continuously.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/buggy_coder' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/http_abhinav' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/abhinav-io' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/CodeinCommand' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Abhinav?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         View social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me or you got something for me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">ℍ𝔼𝕃𝕃𝕆 𝕎𝕆ℝ𝕃𝔻</span>',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
